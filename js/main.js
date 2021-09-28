8e836307fd7921ecad5a10f2336e8863388d6b4a
ddanys08 <denys.gritsaiuk@gmail.com>
2021-09-28 17:35:15 +0000

    Added initial html

--- js/main.js
+++ js/main.js
@@ -1,44 +1,18 @@
-(function () {
-const slides = [
-    '<img src="img/car1.webp" alt="Available car" >',
-    '<img src="img/car2.webp" alt="Available car" >',
-    '<img src="img/car3.webp" alt="Available car" >',
-    '<img src="img/car4.webp" alt="Available car" >',
-    '<img src="img/car5.webp" alt="Available car" >',
-    '<img src="img/car6.webp" alt="Available car" >',
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
 
-  ];
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 350;
+    if (offset > 1248) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
 
-  let currentSlide = 0;
-
-  function showCurrentSlide() {
-    const slideContainer = document.querySelector(".carousel_container .content_container");
-    let html = '';
-    html = slides[currentSlide];
-    const nextSlide = currentSlide + 1 < slides.length ? currentSlide + 1 : 0;
-    html += slides[nextSlide];
-    const next2Slide = nextSlide + 1 < slides.length ? nextSlide + 1 : 0;
-    html += slides[next2Slide];
-    slideContainer.innerHTML = html;
-  }
-
-  function nextSlide() {
-    currentSlide++;
-    if (currentSlide >= slides.length) currentSlide = 0;
-    showCurrentSlide();
-  }
-
-  function prevSlide() {
-    currentSlide--;
-    if (currentSlide < 0) currentSlide = slides.length - 1;
-    showCurrentSlide();
-  }
-
-  showCurrentSlide();
-
-  const btnNext = document.querySelector(".slide-next");
-    btnNext.addEventListener("click", nextSlide);
-  
-    const btnPrev = document.querySelector(".slide-prev");
-    btnPrev.addEventListener("click", prevSlide);
-})();
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 350;
+    if (offset < 0) {
+        offset = 1048;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
